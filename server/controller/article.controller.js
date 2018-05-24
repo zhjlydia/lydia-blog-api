61f094de102286e2ab2ed37825de3d83033a8136
huijuan.zhou <huijuan.zhou@greedyint.cn>
2018-05-24 19:34:32 +0800

    数据库查询初试

--- server/controller/article.controller.js
+++ server/controller/article.controller.js
@@ -4,3 +4,28 @@ const mongoose = require('mongoose')
 const Article = mongoose.model('Article')
 const User = mongoose.model('User')
 const Comment = mongoose.model('Comment')
+
+/**
+ * @method
+ * @param {Type} data 目标对象
+ * @returns {Type} 运营商名称
+ * @desc 前台获取文章列表
+ */
+exports.getFrontArticleList = async(ctx, next) => {
+    let currrentPage = ctx.query.currrentPage > 0
+        ? ctx.query.currrentPage
+        : 1;
+    let pageSize = ctx.query.pageSize > 0
+        ? ctx.query.pageSize
+        : 10;
+    let startRow = (currrentPage - 1) * pageSize;
+    let sort = ctx.query.sortType || "publish_time";
+    // let condition={}; if(ctx.query.tagId){     condition={}; }
+    const list = await Article
+        .find()
+        .select("title images tags visit_count comment_count like_count publish_time")
+        .skip(startRow)
+        .limit(pageSize)
+        .sort(sort)
+
+}
--- server/routes/api/article.js
+++ server/routes/api/article.js
@@ -1,2 +1,5 @@
 const router = require('koa-router')();
 const controller =require('../../controller/article.controller.js');
+
+//前台获取文章列表
+router.get('./getFrontArticleList',controller.getFrontArticleList)
